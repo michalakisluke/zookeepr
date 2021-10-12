c4932599696f854d42d70b967e36e489f5f170a3
Luke Michalakis <michalakisluke@gmail.com>
2021-10-12 11:20:39 -0400

    Fix server load

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 // Provide a file path to a location in the app (Public folder), and makes
 // them into static resources, meaning it can be accessed without a specific
 // endpoint
-app.use(express.static('public'));
+//app.use(express.static('public'));
 //const { animals } = require('./data/animals.json');
 
 app.listen(PORT, () =>{
